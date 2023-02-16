d78676ab466c99aa96daa0a3f9207920d4748c19
Chris Friemann <github@chris.friemann.nl>
2023-02-16 22:29:22 +0100

    fix: port to 3000 and added sass, if right

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sass from 'sass'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass
+      }
     }
   }
 })
