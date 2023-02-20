5e4d5c18075729a3baf5d1acec7797503967f363
Chris Friemann <github@chris.friemann.nl>
2023-02-20 10:21:26 +0100

    feat: fixed scss, using of lets/consts and made example/testComponent

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './assets/scss/variables.scss';
+import './assets/scss/globals.scss';
 
 const app = createApp(App)
 
