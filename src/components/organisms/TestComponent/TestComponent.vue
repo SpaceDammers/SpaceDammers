f2c0444632d912ea7ec060ee550f9bab70bca764
Chris Friemann <github@chris.friemann.nl>
2023-03-06 11:32:30 +0100

    feat: added testpage for designs

--- src/components/organisms/TestComponent/TestComponent.vue
+++ src/components/organisms/TestComponent/TestComponent.vue
@@ -14,6 +14,19 @@ export default defineComponent({
 
 <template>
   <h1>{{ message }}</h1>
+  <h1>This is h1</h1>
+  <h2>This is h2</h2>
+  <h3>This is h3</h3>
+  <h4>This is h4</h4>
+  <h5>This is h5</h5>
+  <h6>This is h6</h6>
+  <p>This is p</p>
+  <a href="#">This is link</a><br/>
+  <button class="button">This is button</button><br />
+  <div class="landstede-rose"></div>
+  <div class="orange"></div>
+  <hr>
+  
 </template>
 
 <style lang="scss">
