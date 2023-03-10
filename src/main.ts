03f154225a83698a83b189f613190969f7498379
Chris Friemann <github@chris.friemann.nl>
2023-03-10 13:05:04 +0100

    feat: added Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -6,7 +7,9 @@ import './assets/scss/variables.scss';
 import './assets/scss/globals.scss';
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
