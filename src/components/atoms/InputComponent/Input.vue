3eae033f927cbe0d6c35495202fc8ec665ad0be7
Chris Friemann <github@chris.friemann.nl>
2023-03-17 12:05:39 +0100

    feat: Input

---
+++ src/components/atoms/InputComponent/Input.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+  name: "InputComponent",
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    orange: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    maxCharacters: {
+      type: Number,
+      default: -1,
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    class="input"
+    :class="{ orange: orange, disabled: disabled }"
+    :placeholder="placeholder"
+    :maxlength="maxCharacters"
+  />
+</template>
+
+<style lang="scss" scoped>
+@import "@atoms/InputComponent/Input.scss";
+</style>
