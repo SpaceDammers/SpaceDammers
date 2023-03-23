a1d533452451b7123548de5e02f950561d64411c
Chris Friemann <github@chris.friemann.nl>
2023-03-23 10:59:35 +0100

    refactor: join room when none exists, one person is in or dont join room when full

--- server/index.ts
+++ server/index.ts
@@ -15,6 +15,7 @@ const io = new Server(httpServer, {
     },
 });
 
+// When a user connects to the server
 io.on("connection", (socket) => {
     console.log("Socket.io user with id:", socket.id, "connected");
 
@@ -23,14 +24,29 @@ io.on("connection", (socket) => {
     });
 });
 
+// When a user joins a room
 io.on("connection", function (socket) {
     socket.on("join", function (roomName: string) {
-        socket.join(roomName.toLowerCase());
-        socket.emit("joined", roomName.toLowerCase());
-        console.log("Socket.io user with id:", socket.id, "joined room:", roomName);
+        // if there are 2 users in the room, don't let the user join
+        if (io.sockets.adapter.rooms.get(roomName.toLowerCase())?.size === 2) {
+            socket.emit("full", roomName.toLowerCase());
+            console.log("Socket.io user with id:", socket.id, "tried to join full room:", roomName);
+            return false;
+        } else if (io.sockets.adapter.rooms.get(roomName.toLowerCase())?.size === 1 || io.sockets.adapter.rooms.get(roomName.toLowerCase())?.size === 0) {
+            socket.join(roomName.toLowerCase());
+            socket.emit("joined", roomName.toLowerCase());
+            console.log("Socket.io user with id:", socket.id, "joined room:", roomName);
+            return true;
+        } else if (io.sockets.adapter.rooms.get(roomName.toLowerCase())?.size === undefined) {
+            socket.join(roomName.toLowerCase());
+            socket.emit("joined", roomName.toLowerCase());
+            console.log("Socket.io user with id:", socket.id, "created room:", roomName);
+            return true;
+        }
     });
 });
 
+// Webclient backend
 app.get("/", (req: Request, res: Response) => {
     res.status(200);
 });
