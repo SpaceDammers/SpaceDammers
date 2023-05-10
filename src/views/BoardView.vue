09e66e90ccd686a6af1a6a83ed859e3302aa3245
Chris Friemann <github@chris.friemann.nl>
2023-05-10 20:54:47 +0200

    feat: trying to fix socketio

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -35,28 +35,33 @@
   import DamCount from '../components/organisms/DamCount/DamCount.vue';
   import BordComponent from '../components/organisms/BordComponent/BordComponent.vue';
   import Chat from '../components/organisms/ChatComponent/Chat2.vue';
-
-  // import { useBordStore } from "../stores/bord";
-  import io from "socket.io-client";
-  const socket = io("http://localhost:3000");
+  import { useBordStore } from "../stores/bord";
+  import SocketioService from "../services/socketio.service";
+  // import io from "socket.io-client";
+  // const socket = io("http://localhost:3000");
 
   export default {
     name: 'BoardView',
     components: { Scoreboard, SmackedPieces, DamCount, BordComponent, Chat },
-    // setup() {
-    //   const bord = useBordStore();
-    //   return { bord };
-    // },
-    // methods: {
-    //   resetBoard(resetFromOther) {
-    //     // Check eerst of dit bord wordt gereset door de andere speler
-    //     if (!resetFromOther) {
-    //       socket.emit("reset");
-    //     }
-    //     // Reset bord
-    //     this.bord.$reset();
-    //   },
-    // }
+    setup() {
+      const bord = useBordStore();
+      const socket = new SocketioService();
+      return { bord, socket };
+    },
+    methods: {
+      resetBoard(resetFromOther) {
+        // // Check eerst of dit bord wordt gereset door de andere speler
+        if (!resetFromOther) {
+          this.socket.resetGame();
+        }
+        // Reset bord
+        this.bord.$reset();
+      },
+    },
+    created() {
+      // Onreset
+      this.socket.onReset();
+    }
   };
 </script>
 <style scoped>
