eac5536a7b792a444e6ac206f66eb3d900d568e9
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2023-05-12 21:21:44 +0200

    fix: all hardcoded socket endpoints to .env

--- src/components/atoms/CheckerPiece/CheckerPiece.vue
+++ src/components/atoms/CheckerPiece/CheckerPiece.vue
@@ -4,7 +4,11 @@
 <script>
 import { useBordStore } from "../../../stores/bord";
 import io from "socket.io-client";
-const socket = io("http://localhost:4000");
+
+// Get socket url from .env file
+const socket = io(process.env.VUE_APP_SOCKET_URL || "http://localhost:4000");
+
+// const socket = io("http://localhost:4000");
 export default {
   name: "CheckerPiece",
   props: ["squareContent"],
